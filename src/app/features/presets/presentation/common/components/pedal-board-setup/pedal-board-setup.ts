4965b541a4d2c33b2db609f402e1e6f792ceeb54
XanderL2 <alexanderllave2021@gmail.com>
2026-08-05 16:14:07 +0200

    Feat: Pedal board setup - in / out indicators

--- src/app/features/presets/presentation/common/components/pedal-board-setup/pedal-board-setup.ts
+++ src/app/features/presets/presentation/common/components/pedal-board-setup/pedal-board-setup.ts
@@ -2,10 +2,12 @@ import { ChangeDetectionStrategy, Component, input, output } from '@angular/core
 import { PresetBlock } from '../../../../domain/entities/preset-entities';
 import { EffectBlock } from "../effect-block/effect-block";
 import { UNLOCK_PRESET_ROUTES } from '../../../../preset.routes';
+import { Icon } from "../../../../../../shared/components/icons/icon/icon";
+import { ASSET_PATHS } from '../../../../../../core/constants/assets-paths';
 
 @Component({
   selector: 'app-pedal-board-setup',
-  imports: [EffectBlock],
+  imports: [EffectBlock, Icon],
   templateUrl: './pedal-board-setup.html',
   styleUrl: './pedal-board-setup.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,6 +20,8 @@ export class PedalBoardSetup {
   // Outputs:
   blockTapped = output<PresetBlock>();
 
+  // Properties:
+  feedBackIconsPath = ASSET_PATHS.icons.feedback;
 
 
   onBlockTapped(blockTapped: PresetBlock): void {
